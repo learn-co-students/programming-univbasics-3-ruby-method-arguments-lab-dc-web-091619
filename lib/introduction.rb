d85e61641eb6d673e346a11f7f2867bc6a496f4f
Charles DeLeo <charles.deleo979@gmail.com>
2019-08-09 21:44:21 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,21 +3,13 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-#introduction 
-def greeting
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-#introduction_with_language
-def greeting
+def introduction(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-#introduction_with_language_optional
-def greeting
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
 
 
 
